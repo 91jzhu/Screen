53a77ec3093ed349efc8c6f5f9a2ac30a74a166d
91jzhu <j1712032377@163.com>
2022-01-24 14:39:40 +0800

    重构 Chart1

---
+++ src/charts/Chart1.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useRef} from "react";
+import * as echarts from "echarts";
+
+const px = (n) => n / 1600 * (window as any).pageWidth
+const Chart1 = () => {
+    const divRef = useRef(null)
+    useEffect(() => {
+        console.log(divRef.current);
+        let myChart = echarts.init(divRef.current);
+        myChart.setOption({
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                top: '5%',
+                left: 'center',
+            },
+            textStyle: {
+                fontSize: 16
+            },
+            series: [
+                {
+                    type: 'pie',
+                    center: ['50%', '60%'],
+                    radius: ['40%', '70%'],
+                    avoidLabelOverlap: false,
+                    itemStyle: {
+                        borderRadius: 10,
+                        borderColor: '#fff',
+                        borderWidth: 2
+                    },
+                    label: {
+                        show: false,
+                        position: 'center'
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            fontSize: '20',
+                            fontWeight: 'bold'
+                        }
+                    },
+                    labelLine: {
+                        show: false,
+                    },
+                    data: [
+                        {value: 493080, name: '运单总量'},
+                        {value: 164360, name: '订单总量'},
+                        {value: 19653, name: '车辆总数'},
+                    ]
+                }
+            ]
+        })
+    }, [])
+    return (
+        <>
+            <h2>数据总览</h2>
+            <div className='chart' ref={divRef}/>
+        </>
+    )
+}
+export {Chart1}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,66 +2,15 @@ import React, {useEffect, useRef} from 'react';
 import './home.scss';
 import {Header} from "../components/Header";
 import * as echarts from 'echarts'
+import {Chart1} from "../charts/Chart1";
 
 export const Home = () => {
-    const divRef = useRef(null)
-    useEffect(() => {
-        console.log(divRef.current);
-        let myChart = echarts.init(divRef.current);
-        myChart.setOption({
-                tooltip: {
-                    trigger: 'item'
-                },
-                legend: {
-                    top: '5%',
-                    left: 'center',
-                },
-                textStyle:{
-                    fontSize:16
-                },
-                series: [
-                    {
-                        type: 'pie',
-                        center:['50%','60%'],
-                        radius: ['40%', '70%'],
-                        avoidLabelOverlap: false,
-                        itemStyle: {
-                            borderRadius: 10,
-                            borderColor: '#fff',
-                            borderWidth: 2
-                        },
-                        label: {
-                            show: false,
-                            position: 'center'
-                        },
-                        emphasis: {
-                            label: {
-                                show: true,
-                                fontSize: '20',
-                                fontWeight: 'bold'
-                            }
-                        },
-                        labelLine: {
-                            show: false,
-                        },
-                        data: [
-                            {value: 493080, name: '运单总量'},
-                            {value: 164360, name: '订单总量'},
-                            {value: 19653, name: '车辆总数'},
-                        ]
-                    }
-                ]
-        })
-    }, [])
     return (
         <div className='wrapper'>
             <Header/>
             <main>
                 <section className='section1'>
-                    <h2>数据总览</h2>
-                    <div className='chart' ref={divRef}>
-
-                    </div>
+                    <Chart1/>
                 </section>
                 <section className='section2'></section>
                 <section className='section3'></section>
