311d1cde5eae56efbaecff7965be189dace74c52
91jzhu <j1712032377@163.com>
2022-01-30 12:37:33 +0800

    重构 Chart10

--- src/charts/Chart10.tsx
+++ src/charts/Chart10.tsx
@@ -1,164 +1,165 @@
 import React, {useEffect, useRef} from "react";
 import * as echarts from "echarts";
 import {randomNum} from "../lib/randomNum";
+import {ChartOption} from "../components/ChartOption";
 
-const px = (n) => n / 1600 * (window as any).pageWidth
-
-
+let flash, myChart
 const Chart10 = () => {
     const divRef = useRef(null)
-    useEffect(() => {
-        let myChart = echarts.init(divRef.current);
-        setInterval(()=>{
-            myChart.setOption({
-                tooltip: {
-                    trigger: 'axis',
+    const setOption = () => {
+        myChart.setOption({
+            tooltip: {
+                trigger: 'axis',
+            },
+            legend: {
+                left: '16%',
+                itemWidth: 8,
+                itemHeight: 4,
+                textStyle: {
+                    color: '#d4d6e2'
                 },
-                legend: {
-                    left: '16%',
-                    itemWidth: 8,
-                    itemHeight: 4,
-                    textStyle: {
+                data: ['车辆总数', '运输中车辆', '使用率'],
+            },
+            grid: {
+                x: 4,
+                x2: 6,
+                y: 28,
+                y2: 0,
+                containLabel: true
+            },
+            xAxis: [
+                {
+                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+                    axisPointer: {
+                        type: 'shadow'
+                    },
+                    splitLine: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLabel: {
+                        show: true,
+                        color: '#d4d6e2',
+                        interval: 0,
+                    }
+                }
+            ],
+            yAxis: [
+                {
+                    min: 7850,
+                    max: 8300,
+                    interval: 150,
+                    name: '(千辆)        ',
+                    nameTextStyle: {
+                        verticalAlign: 'top',
                         color: '#d4d6e2'
                     },
-                    data: ['车辆总数', '运输中车辆', '使用率'],
-                },
-                grid: {
-                    x: 4,
-                    x2: 6,
-                    y: 28,
-                    y2: 0,
-                    containLabel: true
-                },
-                xAxis: [
-                    {
-                        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-                        axisPointer: {
-                            type: 'shadow'
-                        },
-                        splitLine: {
-                            show: false
-                        },
-                        axisTick: {
-                            show: false
-                        },
-                        axisLabel: {
-                            show: true,
-                            color: '#d4d6e2',
-                            interval: 0,
-                        }
-                    }
-                ],
-                yAxis: [
-                    {
-                        min: 7850,
-                        max: 8300,
-                        interval: 150,
-                        name: '(千辆)        ',
-                        nameTextStyle: {
-                            verticalAlign: 'top',
-                            color: '#d4d6e2'
-                        },
-                        nameLocation: 'end',
-                        nameGap: 24,
-                        axisLabel: {
-                            color: '#d4d6e2',
-                            formatter: (value) => {
-                                return value * 1.0 / 1000
-                            }
-                        },
-                        axisLine: {
-                            show: true,
-                            lineStyle: {
-                                color: 'rgba(84, 112, 198,0.35)'
-                            }
-                        },
-                        splitLine: {
-                            show: false
+                    nameLocation: 'end',
+                    nameGap: 24,
+                    axisLabel: {
+                        color: '#d4d6e2',
+                        formatter: (value) => {
+                            return value * 1.0 / 1000
                         }
                     },
-                    {
-                        min: 90,
-                        max: 100,
-                        interval: 2,
-                        name: '          (%)',
-                        nameTextStyle: {
-                            verticalAlign: 'top',
-                            color: '#d4d6e2',
-                        },
-                        axisLabel: {
-                            color: '#d4d6e2'
-                        },
-                        axisLine: {
-                            show: true,
-                            lineStyle: {
-                                color: 'rgba(84, 112, 198,0.35)'
-                            }
-                        },
-                        splitLine: {
-                            show: false
-                        },
-                        nameGap: 24,
-                        nameLocation: 'end',
-                    }
-                ],
-                series: [
-                    {
-                        name: '车辆总数',
-                        type: 'bar',
-                        data: [
-                            randomNum(7850,8200),
-                            randomNum(7850,8200),
-                            randomNum(7850,8200),
-                            randomNum(7850,8200),
-                            randomNum(7850,8200),
-                            randomNum(7850,8200),
-                            randomNum(7850,8200)
-                        ]
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            color: 'rgba(84, 112, 198,0.35)'
+                        }
                     },
-                    {
-                        name: '运输中车辆',
-                        type: 'bar',
-                        data: [
-                            randomNum(7900,8290),
-                            randomNum(7900,8290),
-                            randomNum(7900,8290),
-                            randomNum(7900,8290),
-                            randomNum(7900,8290),
-                            randomNum(7900,8290),
-                            randomNum(7900,8290)
-                        ]
-                    },
-                    {
-                        name: '使用率',
-                        type: 'line',
-                        lineStyle:{
-                            color:'#20f6f7'
-                        },
-                        itemStyle:{
-                            color:'#0dc1fb',
-                            borderColor:'white',
-                            borderType:'dotted'
-                        },
-                        yAxisIndex: 1,
-                        data: [
-                            randomNum(92,99),
-                            randomNum(92,99),
-                            randomNum(92,99),
-                            randomNum(92,99),
-                            randomNum(92,99),
-                            randomNum(92,99),
-                            randomNum(92,99),
-                        ]
+                    splitLine: {
+                        show: false
                     }
-                ]
-            })
-        },1000)
+                },
+                {
+                    min: 90,
+                    max: 100,
+                    interval: 2,
+                    name: '          (%)',
+                    nameTextStyle: {
+                        verticalAlign: 'top',
+                        color: '#d4d6e2',
+                    },
+                    axisLabel: {
+                        color: '#d4d6e2'
+                    },
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            color: 'rgba(84, 112, 198,0.35)'
+                        }
+                    },
+                    splitLine: {
+                        show: false
+                    },
+                    nameGap: 24,
+                    nameLocation: 'end',
+                }
+            ],
+            series: [
+                {
+                    name: '车辆总数',
+                    type: 'bar',
+                    data: [
+                        randomNum(7850,8200),
+                        randomNum(7850,8200),
+                        randomNum(7850,8200),
+                        randomNum(7850,8200),
+                        randomNum(7850,8200),
+                        randomNum(7850,8200),
+                        randomNum(7850,8200)
+                    ]
+                },
+                {
+                    name: '运输中车辆',
+                    type: 'bar',
+                    data: [
+                        randomNum(7900,8290),
+                        randomNum(7900,8290),
+                        randomNum(7900,8290),
+                        randomNum(7900,8290),
+                        randomNum(7900,8290),
+                        randomNum(7900,8290),
+                        randomNum(7900,8290)
+                    ]
+                },
+                {
+                    name: '使用率',
+                    type: 'line',
+                    lineStyle:{
+                        color:'#20f6f7'
+                    },
+                    itemStyle:{
+                        color:'#0dc1fb',
+                        borderColor:'white',
+                        borderType:'dotted'
+                    },
+                    yAxisIndex: 1,
+                    data: [
+                        randomNum(92,99),
+                        randomNum(92,99),
+                        randomNum(92,99),
+                        randomNum(92,99),
+                        randomNum(92,99),
+                        randomNum(92,99),
+                        randomNum(92,99),
+                    ]
+                }
+            ]
+        })
+    }
+    useEffect(() => {
+        myChart = echarts.init(divRef.current)
+        flash = ChartOption(setOption)
+        flash.Leave()
     }, [])
     return (
         <>
             <h2>上周车辆使用情况</h2>
-            <div className='chart' ref={divRef}/>
+            <div className='chart' ref={divRef} onMouseEnter={()=>flash.Enter()} onMouseLeave={()=>flash.Leave()}/>
         </>
     )
 }
