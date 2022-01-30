567a89eff89aa3ac6d1bf421f8a28459e711fe7a
91jzhu <j1712032377@163.com>
2022-01-30 14:11:08 +0800

    重构 Chart11

--- src/charts/Chart11.tsx
+++ src/charts/Chart11.tsx
@@ -1,42 +1,46 @@
 import React, {useEffect, useRef} from "react";
 import * as echarts from "echarts";
 import {randomNum} from "../lib/randomNum";
+import {ChartOption} from "../components/ChartOption";
 
+let flash, myChart
 const Chart11 = () => {
     const divRef = useRef(null)
-    useEffect(() => {
-        let myChart = echarts.init(divRef.current);
-        setInterval(()=>{
-            myChart.setOption({
-                tooltip: {
-                    show: true,
-                },
-                color: ['#2584f4', '#0ee7ea', '#fa967d'],
-                series: [
-                    {
-                        type: 'pie',
-                        startAngle: -25,
+    const setOption = () => {
+        myChart.setOption({
+            tooltip: {
+                show: true,
+            },
+            color: ['#2584f4', '#0ee7ea', '#fa967d'],
+            series: [
+                {
+                    type: 'pie',
+                    startAngle: -25,
 
-                        radius: [35, 65],
-                        center: ['50%', '50%'],
-                        itemStyle: {
-                            borderWidth: 2,
-                            borderColor: '#0f0a44'
-                        },
-                        data: [
-                            {value: randomNum(10,90), name: '司机'},
-                            {value: randomNum(10,90), name: '承运商'},
-                            {value: randomNum(10,90), name: '货主'}
-                        ]
+                    radius: [35, 65],
+                    center: ['50%', '50%'],
+                    itemStyle: {
+                        borderWidth: 2,
+                        borderColor: '#0f0a44'
                     },
-                ]
-            })
-        },1000)
+                    data: [
+                        {value: randomNum(10,90), name: '司机'},
+                        {value: randomNum(10,90), name: '承运商'},
+                        {value: randomNum(10,90), name: '货主'}
+                    ]
+                },
+            ]
+        })
+    }
+    useEffect(() => {
+        myChart = echarts.init(divRef.current)
+        flash = ChartOption(setOption)
+        flash.Leave()
     }, [])
     return (
         <>
             <h2>注册用户人数占比情况</h2>
-            <div className='chart' ref={divRef}/>
+            <div className='chart' ref={divRef} onMouseEnter={()=>flash.Enter()} onMouseLeave={()=>flash.Leave()}/>
         </>
     )
 }
